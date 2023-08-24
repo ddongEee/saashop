805a4b4f601b0d429e146e17a6e3f837c5ebd64a
Haksung Kim <kmhak@amazon.com>
2023-08-24 09:53:14 +0900

    apply parameter store

---
+++ services/delivery/src/main/java/com/example/saashop/delivery/configuration/AwsGlobalConfig.java
@@ -0,0 +1,14 @@
+package com.example.saashop.delivery.configuration;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsGlobalConfig {
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new DefaultAWSCredentialsProviderChain();
+    }
+}
--- services/payment/src/main/java/com/example/saashop/payment/configuration/SQSConfig.java
+++ services/payment/src/main/java/com/example/saashop/payment/configuration/SQSConfig.java
@@ -1,9 +1,8 @@
-package com.example.saashop.order;
+package com.example.saashop.payment.configuration;
 
 import com.amazon.sdk.spring.common.message.TenantAwareQueueMessagingTemplate;
 import com.amazon.sdk.spring.common.message.TenantAwareThreadPoolTaskExecutor;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
@@ -18,7 +17,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import java.util.Arrays;
 
 @EnableSqs
 @Configuration
@@ -27,33 +25,23 @@ public class SQSConfig {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKeyId;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretAccessKey;
-
     @Value("${cloud.aws.queue.uri}")
     private String sqsUrl;
 
     @Bean
     @Primary
-    public AmazonSQSAsync amazonSQSAsync(Environment env) {
+    public AmazonSQSAsync amazonSQSAsync(Environment env, final AWSCredentialsProvider awsCredentialsProvider) {
         AmazonSQSAsyncClientBuilder amazonSQSAsyncClientBuilder = AmazonSQSAsyncClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(sqsUrl, region))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKeyId, secretAccessKey)));
-        if (Arrays.asList(env.getActiveProfiles()).contains("consumer")) {
-            return amazonSQSAsyncClientBuilder
-                    .build();
-        }
+                .withCredentials(awsCredentialsProvider);
         return amazonSQSAsyncClientBuilder
                 .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder()))
                 .build();
     }
 
     @Bean
-    public TenantAwareQueueMessagingTemplate queueMessagingTemplate(final Environment environment) {
-        return new TenantAwareQueueMessagingTemplate(amazonSQSAsync(environment), environment);
+    public TenantAwareQueueMessagingTemplate queueMessagingTemplate(final Environment environment, final AWSCredentialsProvider awsCredentialsProvider) {
+        return new TenantAwareQueueMessagingTemplate(amazonSQSAsync(environment, awsCredentialsProvider), environment);
     }
 
     @Bean
