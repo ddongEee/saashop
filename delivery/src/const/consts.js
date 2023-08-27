c452e8022fb00f4f31077fb71b985fbf0724311c
Jihye Kwon <geeyaaa@gmail.com>
2023-08-27 22:14:01 +0900

    add delivery service in node express

---
+++ delivery/src/const/consts.js
@@ -0,0 +1,9 @@
+/**
+ * Constants object containing various constant properties.
+ *
+ * @const {object} consts
+ * @property {string} UNKNOWN - Represents an unknown value or default when a specific value is not available.
+ */
+export const consts = {
+    UNKNOWN: 'unknown',
+};
