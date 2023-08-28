bd687bee57cd4543b8cf000a3693d8423150e76e
Jihye Kwon <geeyaaa@gmail.com>
2023-08-28 22:45:39 +0900

    delete delivery in root directory

--- delivery/src/const/consts.js
+++
@@ -1,9 +0,0 @@
-/**
- * Constants object containing various constant properties.
- *
- * @const {object} consts
- * @property {string} UNKNOWN - Represents an unknown value or default when a specific value is not available.
- */
-export const consts = {
-    UNKNOWN: 'unknown',
-};
