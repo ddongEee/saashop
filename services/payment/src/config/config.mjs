0af5df7bfe4b4f6837538d9ad7e6ed7bd0948383
Suyeon Kim <ksuyeon70@google.com>
2023-08-29 01:01:28 +0900

    update payment service

--- services/payment/src/config/config.mjs
+++ services/payment/src/config/config.mjs
@@ -1,33 +1,26 @@
 const LOG_CONFIG = {
-    enableLogging: true,
-    logOptions: {
-      ENABLE_XRAY_TRACE_ID: true,
-      ENABLE_CALLER: true,
-      CALLER_PREFIX: "lambda:"
-    },
-    logConst: {
-      UNKNOWN: "unknown",
-      LOG_CONTEXT_FROM_TOKEN_KEY: "headers.authorization",
-      LOG_CONTEXT_FROM_LAMBDA_KEY: "_logContext",
-      LOG_CONTEXT_FROM_SQS_KEY: "messageAttributes.logAttributes.stringValue",
-      LOG_CONTEXT_FROM_ECS_KEY: "_logContext",
-      SET_CONTEXT_INTO_LAMBDA_PAYLOAD_KEY: "_logContext"
-    },
-    headerLogFormat: {
-    //   "tenant.id": "tenantContext.tenantId",
-    //   "tenant.name": "tenantContext.tenantName",
-    //   "tenant.tier": "tenantContext.tier",
-    //   "user.userId": "userContext.userId",
-    //   "user.gender": "userContext.gender",
-    //   "user.role": "userContext.userRole",
-    //   "user.country": "userContext.country",
-        "organizations[0].organizationId": "tenantContext.tenantId",
-        "organizations[0].organizationName": "tenantContext.tenantName",
-        "account.userId": "userContext.userId",
-        "user.gender": "userContext.gender",
-        "lastPlace.roleId": "userContext.userRole"
-    }
-  }
-    
-  export default LOG_CONFIG;
-    
+  enableLogging: true,
+  logOptions: {
+    ENABLE_XRAY_TRACE_ID: true,
+    ENABLE_CALLER: true,
+    CALLER_PREFIX: 'lambda:',
+  },
+  logConst: {
+    UNKNOWN: 'unknown',
+    LOG_CONTEXT_FROM_TOKEN_KEY: 'headers.authorization',
+    LOG_CONTEXT_FROM_LAMBDA_KEY: '_logContext',
+    LOG_CONTEXT_FROM_SQS_KEY: 'messageAttributes.logAttributes.stringValue',
+    LOG_CONTEXT_FROM_ECS_KEY: '_logContext',
+    SET_CONTEXT_INTO_LAMBDA_PAYLOAD_KEY: '_logContext',
+  },
+  headerLogFormat: {
+    'tenant.tenantId': 'tenantContext.tenantId',
+    'tenant.tenantName': 'tenantContext.tenantName',
+    'tenant.plan': 'tenantContext.tier',
+    'user.userId': 'userContext.userId',
+    'user.gender': 'userContext.gender',
+    'user.role': 'userContext.userRole',
+  },
+};
+
+export default LOG_CONFIG;
--- services/payment/src/service/service.mjs
+++ services/payment/src/service/service.mjs
@@ -1,70 +1,70 @@
-import AWSXRay from "aws-xray-sdk";
-import { SSMClient, GetParametersCommand } from "@aws-sdk/client-ssm";
-import { DynamoDBClient, UpdateItemCommand }from '@aws-sdk/client-dynamodb';
-// import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
-import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import AWSXRay from 'aws-xray-sdk';
+import { SSMClient, GetParametersCommand } from '@aws-sdk/client-ssm';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
 
 const region = process.env.AWS_REGION;
 // const ssmClient = AWSXRay.captureAWSv3Client(new SSMClient({region: region}));
 // const sqsClient = AWSXRay.captureAWSv3Client(new SQSClient({region: region}));
 // const ddbClient = AWSXRay.captureAWSv3Client(new DynamoDBClient({region: region}));
-const ssmClient = new SSMClient({region: region});
-const sqsClient = new SQSClient({region: region});
-const ddbClient = new DynamoDBClient({region: region});
+const ssmClient = new SSMClient({ region: region });
+const sqsClient = new SQSClient({ region: region });
+const ddbClient = new DynamoDBClient({ region: region });
 const docClient = DynamoDBDocumentClient.from(ddbClient);
 
 const SET_SQS_MSG_ATTRIBUTES_KEY = 'logAttributes';
 const SSM_PARAMS_KEY = {
-//   DDB_TABLE_NAME: '/summit/app/cloud.aws.ddb.table-name',
-//   QUEUE_URI: '/summit/app/cloud.aws.queue.uri',
-//   PAID_QUEUE_NAME: '/summit/app/cloud.aws.queue.paid-name',
-//   REGION: '/summit/app/cloud.aws.region.static'
+  //   DDB_TABLE_NAME: '/summit/app/cloud.aws.ddb.table-name',
+  //   QUEUE_URI: '/summit/app/cloud.aws.queue.uri',
+  //   PAID_QUEUE_NAME: '/summit/app/cloud.aws.queue.paid-name',
+  //   REGION: '/summit/app/cloud.aws.region.static'
   DDB_TABLE_NAME: '/ksuyeon/ddb.table-name',
   QUEUE_URI: '/ksuyeon/queue.uri',
   PAID_QUEUE_NAME: '/ksuyeon/queue.paid-name',
-  REGION: '/ksuyeon/region.static'
-}
+  REGION: '/ksuyeon/region.static',
+};
 let RESOURCE_PARAMS = {};
 
 const getResourceParameters = async () => {
-  const input = { // GetParametersRequest
+  const input = {
+    // GetParametersRequest
     Names: Object.values(SSM_PARAMS_KEY), // ParameterNameList
     WithDecryption: false,
   };
   const command = new GetParametersCommand(input);
-  const parameters = (await ssmClient.send(command)).Parameters;  
+  const parameters = (await ssmClient.send(command)).Parameters;
   console.log('>> getResourceParameters(): ', parameters);
 
   const parameterMappings = {
-    [SSM_PARAMS_KEY.DDB_TABLE_NAME]: "DDB_TABLE_NAME",
-    [SSM_PARAMS_KEY.QUEUE_URI]: "QUEUE_URI",
-    [SSM_PARAMS_KEY.PAID_QUEUE_NAME]: "PAID_QUEUE_NAME",
-    [SSM_PARAMS_KEY.REGION]: "REGION"
+    [SSM_PARAMS_KEY.DDB_TABLE_NAME]: 'DDB_TABLE_NAME',
+    [SSM_PARAMS_KEY.QUEUE_URI]: 'QUEUE_URI',
+    [SSM_PARAMS_KEY.PAID_QUEUE_NAME]: 'PAID_QUEUE_NAME',
+    [SSM_PARAMS_KEY.REGION]: 'REGION',
   };
-  
+
   for (const parameter of parameters) {
     const paramName = parameterMappings[parameter.Name];
     if (paramName) {
       RESOURCE_PARAMS[paramName] = parameter.Value;
     }
   }
-  
+
   return RESOURCE_PARAMS;
 };
 
 const appendMessageAttributes = (originParam, key, value) => {
-    const newParams = {
-      ...originParam,
-      MessageAttributes: {
-        [key]: {
-          DataType: 'String',
-          StringValue: JSON.stringify(value),
-        },
+  const newParams = {
+    ...originParam,
+    MessageAttributes: {
+      [key]: {
+        DataType: 'String',
+        StringValue: JSON.stringify(value),
       },
-      ...originParam.MessageAttributes,
-    };
-    return newParams ?? undefined;
+    },
+    ...originParam.MessageAttributes,
+  };
+  return newParams ?? undefined;
 };
 
 const sendMessage = async (params, context = undefined) => {
@@ -92,6 +92,6 @@ const updateOrderStatus = async (params) => {
   const response = await docClient.send(command);
   console.log(response);
   return response;
-}
+};
 
-export { getResourceParameters, updateOrderStatus, sendMessage }
+export { getResourceParameters, updateOrderStatus, sendMessage };
