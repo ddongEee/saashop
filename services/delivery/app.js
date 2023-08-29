1066652481bb81ac8171cd61b380159b227f5684
Haemi Kim <haemikim@amazon.com>
2023-08-29 12:09:32 +0900

    Add delivery service to cdk

--- services/delivery/app.js
+++ services/delivery/app.js
@@ -1,22 +1,28 @@
-
 import { logger, ContextAwareSqsConsumer, ContextAwareSqsProducer } from '@hdall/express';
 import { v4 as uuidv4 } from 'uuid';
-import { DynamoDBClient, UpdateItemCommand, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb"; 
-import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { DynamoDBClient, UpdateItemCommand, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
 import { consts } from './src/const/consts.js';
 
 const ssmClient = new SSMClient();
 const ddbClient = new DynamoDBClient();
 // const producer = new ContextAwareSqsProducer();
 
-const getConsumeSqsUrlInput = { 
-  Name: consts.CONSUME_TARGET_SQS, 
+const getConsumeSqsUriInput = {
+  Name: consts.CONSUME_TARGET_SQS_URI,
 };
-const response1 = await ssmClient.send(new GetParameterCommand(getConsumeSqsUrlInput));
-const fromSqsUrl = response1.Parameter.Value;
 
-const getDdbTableInput = { 
-  Name: consts.TARGET_DDB_TABLE, 
+const getConsumeSqsNameInput = {
+  Name: consts.CONSUME_TARGET_SQS_NAME,
+};
+const sqsUriRes = await ssmClient.send(new GetParameterCommand(getConsumeSqsUriInput));
+const sqsNameRes = await ssmClient.send(new GetParameterCommand(getConsumeSqsNameInput));
+const sqsUri = sqsUriRes.Parameter.Value;
+const sqsName = sqsNameRes.Parameter.Value;
+const fromSqsUrl = sqsUri + '/' + sqsName;
+
+const getDdbTableInput = {
+  Name: consts.TARGET_DDB_TABLE,
 };
 const response2 = await ssmClient.send(new GetParameterCommand(getDdbTableInput));
 const ddbTableName = response2.Parameter.Value;
@@ -35,38 +41,38 @@ const getCurrentFormattedDate = () => {
   const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
 
   return formattedDate;
-}
+};
 
 const customHandleMessage = async (m) => {
   try {
-    console.log(`\n=========== ${fromSqsUrl} CONSUMER customHandleMessage =====================>`)
+    console.log(`\n=========== ${fromSqsUrl} CONSUMER customHandleMessage =====================>`);
     // console.info('Hello from jihyeRay1 listener > ', m);
 
     const attributes = m.Attributes;
-    const orderId = "a50a1a1d-e1b2-4b12-8223-68357703cee4"; //attributes.orderId ?? uuidv4();
+    const orderId = 'a50a1a1d-e1b2-4b12-8223-68357703cee4'; //attributes.orderId ?? uuidv4();
     // const orderId = attributes.orderId ?? uuidv4();
     logger.info('orderId > ', orderId);
 
     const ddbGetInput = {
-      "TableName": ddbTableName,
-      "Key": {
-        "orderId": {
-          "S": orderId
+      TableName: ddbTableName,
+      Key: {
+        orderId: {
+          S: orderId,
         },
       },
     };
 
     const existedOrder = await ddbClient.send(new GetItemCommand(ddbGetInput));
-    console.log("@ existedOrder > ", existedOrder.Item)
+    console.log('@ existedOrder > ', existedOrder.Item);
 
     let ddbInput;
     const current = getCurrentFormattedDate();
-    if(existedOrder.Item) {
+    if (existedOrder.Item) {
       ddbInput = {
         TableName: ddbTableName,
         Key: {
-          "orderId": {
-            "S": orderId
+          orderId: {
+            S: orderId,
           },
           // "event": {
           //   "L": beforeEvent.push({ "M": { "DELIVERED" : {"S": getCurrentFormattedDate()}}})
@@ -78,10 +84,10 @@ const customHandleMessage = async (m) => {
         UpdateExpression: 'SET event = list_append(event, :deliveredEvent), lastUpdatedAt = :currentTime',
         ExpressionAttributeValues: {
           ':deliveredEvent': {
-            L: [{ M : { DELIVERED: { S: current} }}]
+            L: [{ M: { DELIVERED: { S: current } } }],
           },
           ':currentTime': {
-            S: current
+            S: current,
           },
         },
         // AttributeUpdates: {
@@ -94,33 +100,29 @@ const customHandleMessage = async (m) => {
         //     Action: "PUT"
         //   }
         // },
-        ReturnValues: "ALL_NEW",
-      }
+        ReturnValues: 'ALL_NEW',
+      };
 
       const response4 = await ddbClient.send(new UpdateItemCommand(ddbInput));
-      console.log("@ UpdateItemCommand result >> ", response4);
+      console.log('@ UpdateItemCommand result >> ', response4);
     } else {
       ddbInput = {
         TableName: ddbTableName,
         Item: {
-          "orderId": {
-            "S": orderId
+          orderId: {
+            S: orderId,
           },
-          "lastUpdatedAt": {
-            "S": current
+          lastUpdatedAt: {
+            S: current,
           },
-          "event": {
-            "L": [
-              { M : {DEIVERED: {S: current} }}
-            ]
-          }
-        }
-      }
+          event: {
+            L: [{ M: { DEIVERED: { S: current } } }],
+          },
+        },
+      };
       const response5 = await ddbClient.send(new PutItemCommand(ddbInput));
-      console.log("@ PutItemCommand >> ", response5);
+      console.log('@ PutItemCommand >> ', response5);
     }
-
-    
 
     // const data = {
     //   orderId: orderId,
@@ -133,11 +135,10 @@ const customHandleMessage = async (m) => {
 
     // const response = await producer.tenantAwareSendMessage(params);
     // console.info(`================== SEND COMPLETED message to ${nextSqsUrl}>>>`, response)
-
   } catch (e) {
     logger.error(e, e.stack);
   }
-}
+};
 
 const sqsConsumer = ContextAwareSqsConsumer.create({
   messageAttributeNames: ['All'],
@@ -150,7 +151,6 @@ const sqsConsumer = ContextAwareSqsConsumer.create({
 sqsConsumer.on('error', (err) => {
   logger.error(`error`, err);
 });
-
 
 logger.info(`[init][SQS] polling with ${fromSqsUrl}`);
 sqsConsumer.start();
--- services/delivery/src/const/consts.js
+++ services/delivery/src/const/consts.js
@@ -5,7 +5,8 @@
  * @property {string} UNKNOWN - Represents an unknown value or default when a specific value is not available.
  */
 export const consts = {
-    UNKNOWN: 'unknown',
-    CONSUME_TARGET_SQS: '/jihye/delivery-consuming-sqs',
-    TARGET_DDB_TABLE: '/jihye/saashoptable'
+  UNKNOWN: 'unknown',
+  CONSUME_TARGET_SQS_URI: '/summit/app/cloud.aws.queue.uri',
+  CONSUME_TARGET_SQS_NAME: '/summit/app/cloud.aws.queue.paid-name',
+  TARGET_DDB_TABLE: '/summit/app/cloud.aws.ddb.table-name',
 };
