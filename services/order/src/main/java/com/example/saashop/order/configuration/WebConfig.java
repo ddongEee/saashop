9f00d2eb69ea520a726b60c6fd6182ddf6c30152
Haksung Kim <kmhak@amazon.com>
2023-08-30 10:30:09 +0900

    modified cors

--- services/order/src/main/java/com/example/saashop/order/configuration/WebConfig.java
+++ services/order/src/main/java/com/example/saashop/order/configuration/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(allowedOrigins);
+                registry.addMapping("/**").allowedOrigins("*");
                 WebMvcConfigurer.super.addCorsMappings(registry);
             }
         };
