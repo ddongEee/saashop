65a8128a71380826c67d679e6382a1692ad97292
Jihye Kwon <geeyaaa@gmail.com>
2023-09-03 23:57:19 +0900

    add new folder for express with heimdall

---
+++ services/delivery_with_heimdall/app.js
@@ -0,0 +1,112 @@
+import { logger, ContextAwareSqsConsumer, tracer, ContextAwareSqsProducer, contextMiddleware, openSegment, closeSegment } from '@hdall/express';
+import { DynamoDBClient, UpdateItemCommand, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+import { consts } from './src/const/consts.js';
+import { Consumer } from 'sqs-consumer';
+
+
+const ssmClient = new SSMClient();
+tracer.captureAWSv3Client(ssmClient);
+const ddbClient = new DynamoDBClient();
+tracer.captureAWSv3Client(ddbClient);
+
+const getConsumeSqsUriInput = {
+  Name: consts.CONSUME_TARGET_SQS_URI,
+};
+
+const getConsumeSqsNameInput = {
+  Name: consts.CONSUME_TARGET_SQS_NAME,
+};
+const sqsUriRes = await ssmClient.send(new GetParameterCommand(getConsumeSqsUriInput));
+const sqsNameRes = await ssmClient.send(new GetParameterCommand(getConsumeSqsNameInput));
+const sqsUri = sqsUriRes.Parameter.Value;
+const sqsName = sqsNameRes.Parameter.Value;
+const fromSqsUrl = sqsUri + '/' + sqsName;
+
+const getDdbTableInput = {
+  Name: consts.TARGET_DDB_TABLE,
+};
+const response2 = await ssmClient.send(new GetParameterCommand(getDdbTableInput));
+const ddbTableName = response2.Parameter.Value;
+
+const getCurrentFormattedDate = () => {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+
+  const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+  return formattedDate;
+};
+
+const customHandleMessage = async (m) => {
+  try {
+    logger.info(`\n=========== ${fromSqsUrl} CONSUMER customHandleMessage =====================>`);
+    logger.info('MESSAGE > ', m);
+
+    const body = JSON.parse(m.Body);
+    const orderId = body.orderId;
+
+    const ddbGetInput = {
+      TableName: ddbTableName,
+      Key: {
+        orderId: {
+          S: orderId,
+        },
+      },
+    };
+
+    const existedOrder = await ddbClient.send(new GetItemCommand(ddbGetInput));
+
+    let ddbInput;
+    const current = getCurrentFormattedDate();
+    if (existedOrder.Item) {
+      ddbInput = {
+        TableName: ddbTableName,
+        Key: {
+          orderId: {
+            S: orderId,
+          },
+        },
+        UpdateExpression: 'SET event = list_append(event, :deliveredEvent), lastUpdatedAt = :currentTime',
+        ExpressionAttributeValues: {
+          ':deliveredEvent': {
+            L: [{ M: { DELIVERED: { S: current } } }],
+          },
+          ':currentTime': {
+            S: current,
+          },
+        },
+        ReturnValues: 'ALL_NEW',
+      };
+
+      const response4 = await ddbClient.send(new UpdateItemCommand(ddbInput));
+      logger.info('@ UpdateItemCommand result >> ', response4.Attributes.orderId);
+    } else {
+      logger.info('@ NO ORDER ID >> ', m);
+    }
+  } catch (e) {
+    logger.error(e, e.stack);
+  }
+};
+
+const sqsConsumer = ContextAwareSqsConsumer.create({
+  messageAttributeNames: ['All'],
+  queueUrl: fromSqsUrl,
+  batchSize: 10,
+  waitTimeSeconds: 20,
+  handleMessage: customHandleMessage,
+});
+
+sqsConsumer.on('error', (err) => {
+  logger.error(`error`, err);
+});
+
+logger.info(`[init][SQS] polling with ${fromSqsUrl}`);
+sqsConsumer.start();
---
+++ services/delivery_with_heimdall/src/const/consts.js
@@ -0,0 +1,12 @@
+/**
+ * Constants object containing various constant properties.
+ *
+ * @const {object} consts
+ * @property {string} UNKNOWN - Represents an unknown value or default when a specific value is not available.
+ */
+export const consts = {
+  UNKNOWN: 'unknown',
+  CONSUME_TARGET_SQS_URI: '/summit/app/cloud.aws.queue.uri',
+  CONSUME_TARGET_SQS_NAME: '/summit/app/cloud.aws.queue.paid-name',
+  TARGET_DDB_TABLE: '/summit/app/cloud.aws.ddb.table-name',
+};
